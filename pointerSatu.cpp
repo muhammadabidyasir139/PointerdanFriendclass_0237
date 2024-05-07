d6de8e64bca6fcfee78e114c7919341a0e2d1550
wesclicdev <wesclicdev@gmail.com>
2024-05-07 15:28:40 +0700

    membuat flow menampilakan nim mahasiswa dengan pointer object di pointerdua.cpp

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -6,21 +6,23 @@ class mahasiswa
     public:
         int nim;
 
+         mahasiswa(int n) : nim(n) {}
+
         void showNim() {
             cout << "No induk = " << nim << endl;
         }
 };
 
 int main(){
-    mahasiswa mhs(1); //object mhs
+    mahasiswa mhs{1}; //object mhs
     mhs.showNim(); // member access operator
 
-    mahasiswa &refMhs = mhs;
-    refMhs.nim = 2;
+    mahasiswa &refMhs = mhs; //pointer dereferences pMhs
+    refMhs.nim = 2; //Member Access Opeator
     mhs.showNim();
 
-    mahasiswa *pMhs = &mhs;
-    pMhs->nim = 3;
+    mahasiswa *pMhs = &mhs; //pointer dereferences pMhs
+    pMhs->nim = 3; //Arrow Operator
     pMhs->showNim();
     return 0;
 
